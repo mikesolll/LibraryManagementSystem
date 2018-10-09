de2d0fc416131183b2c59e5111700f6d8e6419bb
Kabinad  Melaku <kabinad.melaku@gmail.com>
2018-10-09 15:23:25 -0500

    login librarina checkout linked

--- src/ui/LibrarianController.java
+++ src/ui/LibrarianController.java
@@ -1,12 +1,33 @@
 package ui;
 
-import Application.main;
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class LibrarianController {
-
-	public void setMainApp(main main) {
-		// TODO Auto-generated method stub
+	private Button bookCheckOut;
+	private Button CheckOutRecord;
+	
+	public void bookCheckOut(){
+		 try {
+		        FXMLLoader fxmlLoader = new FXMLLoader();
+		        fxmlLoader.setLocation(getClass().getResource("/ui/CheckOut.fxml"));
+		        /* 
+		         * if "fx:controller" is not set in fxml
+		         * fxmlLoader.setController(NewWindowController);
+		         */
+		        Scene scene = new Scene(fxmlLoader.load(), 487, 596);
+		        Stage stage = new Stage();
+		        stage.setTitle("New Window");
+		        stage.setScene(scene);
+		        stage.show();
+		    } catch (IOException e) {
+		       // Logger logger = Logger.getLogger(getClass().getName());
+		        //logger.log(Level.SEVERE, "Failed to create new Window.", e);
+		    }
 		
 	}
-
 }
